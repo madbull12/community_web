5aacff606e0119c6241f7d8b1e851eeb2539f259
madbull12 <huangandrian02@gmail.com>
2023-12-12 00:15:39 +0700

    moment js added

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -1,6 +1,7 @@
 import { PostDto } from "@/data-access/posts";
 import React from "react";
 import UserAvatar from "./user-avatar";
+import moment from "moment";
 
 const Post = ({ post }: { post: PostDto }) => {
   console.log(post);
@@ -10,15 +11,13 @@ const Post = ({ post }: { post: PostDto }) => {
         <UserAvatar
           src={post.author.image as string}
           fallback={post?.author?.name?.split("")[0].charAt(0) as string}
-
           className="rounded-full w-6 h-6"
-
         />
-        <span className="text-[12px] text-foreground">Posted by {post.author.name}</span>
+        <span className="text-[12px] text-foreground">
+          Posted by • {post.author.name} {moment(post.createdAt).fromNow()}
+        </span>
       </div>
-      <p>
-        {post.content}
-      </p>
+      <p>{post.content}</p>
     </div>
   );
 };
